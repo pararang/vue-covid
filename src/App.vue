952a597b0b919af84d0b02491e980f8a7b5836bd
Muhammad Ikhsan <pararang@gmail.com>
2020-03-12 20:43:24 +0700

    ignore

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Corona logo" src="./assets/virus_corona.png">
+    <Header title="Informasi Covid 19"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
---
+++ src/components/Current.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ data }}</h1>
+    
+</template>
+
+<script>
+export default {
+    name: 'Current',
+    props: {
+        data: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
