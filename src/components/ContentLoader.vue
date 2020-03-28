849ca2316f2aee0e7526b716ba217455e93aa461
Sapariansyah <rian070989@gmail.com>
2020-03-28 08:33:59 +0700

    Add icon on refresh button

--- src/components/ContentLoader.vue
+++ src/components/ContentLoader.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="content-loader">
         <div style="text-align: right;">
-            <b-button pill @click="refreshData()" variant="primary" :disabled="isLoading">
-                {{!isLoading? 'Muat Ulang': 'Sedang Memuat...'}}
+            <b-button @mouseover="animatedIcon = true" @mouseleave="animatedIcon = false" pill @click="refreshData()" variant="primary" :disabled="isLoading">
+                <b-icon-arrow-clockwise :animation="isLoading || animatedIcon? 'spin': ''"></b-icon-arrow-clockwise> {{!isLoading? 'Muat Ulang': 'Sedang Memuat...'}}
             </b-button>
         </div>
         <template v-if="!isLoading">
@@ -16,9 +16,17 @@
 </template>
 
 <script>
+    import { BIconArrowClockwise } from 'bootstrap-vue'
+
     export default {
         name: "ContentLoader",
+        components: { BIconArrowClockwise },
         props: { isLoading: { default: true } },
+        data() {
+            return {
+                animatedIcon: false
+            }
+        },
         methods: {
             refreshData() {this.$emit('refresh-data')}
         }
